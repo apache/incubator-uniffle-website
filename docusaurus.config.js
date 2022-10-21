29a41147131f30542357b056b2d57cacedc0da65
roryqi <roryqi@tencent.com>
2022-10-21 19:40:10 +0800

    Add href

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,6 +63,10 @@ const config = {
                     showReadingTime: true,
                     editUrl: 'https://github.com/apache/incubator-uniffle',
                 },
+                download: {
+                    showReadingTime: true,
+                    editUrl: 'https://github.com/apache/incubator-uniffle',
+                },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
