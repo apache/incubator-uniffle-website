a567b3808843ba72f676e9bba5207de202b6b717
Kaijie Chen <chen@kaijie.org>
2023-04-10 13:25:20 +0800

    docs: change "Disaggregated Storage" to "Elastic"

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -25,7 +25,7 @@ const FeatureList: FeatureItem[] = [
         ),
     },
     {
-        title: 'Disaggregated Storage',
+        title: 'Elastic',
         description: (
             <>
                 Enables orchestration and improves resource utilization.
